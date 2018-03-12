fixed softmax overflow

Capstone Source Code/GeneticNN/network.py
@@ -56,18 +56,25 @@ class Network:
         self._hyper_param['weights']['2'] = np.random.randn( self._hyper_param['num_hidden_neuron'], 3) / np.sqrt(
             self._hyper_param['num_hidden_neuron'])
     def softmax1(self,vector):
-
         # if(len(x.shape)==1):
         #  x = x[np.newaxis,...]
         probs = np.exp(vector - np.max(vector, axis=1, keepdims=True))
         probs /= np.sum(probs, axis=1, keepdims=True)
         return probs
+
     def softmax(self, vector):
         """Compute softmax values for each sets of scores in vector"""
-        return np.exp(vector) / np.sum(np.exp(vector), axis=0)
+        return np.exp(vector) / np.sum(np.exp(vector))
+
+    def softmax3(self, v):
+        x_exp = np.exp(v)
+        x_sum = np.sum(x_exp, axis=1, keepdims=True)
+        s = x_exp/x_sum
+        return s
 
     def setReward(self, r):
         self.reward = r
+
 
     def predict(self, input_p):
         n1 = input_p.dot(self.getHyperParam()['weights']['1'])

Capstone Source Code/GeneticNN/testnetwork.py
@@ -15,10 +15,10 @@ import gym, pickle
 from helper_methods import preprocess_observation
 from network import Network
 from multiprocessing import Process
+import numpy as np
 
 
-
-
+from workthread import getAction
 def main(hyperParam):
     # load the network and the saved hyper parameters into this network
     _net = Network(hyper_param=hyperParam)
@@ -28,8 +28,14 @@ def main(hyperParam):
     while True:
         env.render()
         pro, prev_obs = preprocess_observation(obs, prev_obs, 80*80)
-        a1_output,a2_output = _net.predict(pro)
-        action = _net.getAction(a2_output)
+        a2,a1 = _net.predict(pro)
+        u = np.random.uniform()
+        print("a2 ", a2)
+        prob_cum = np.cumsum(a2)
+        a = np.where(u <= prob_cum)
+        print("prob_cum " , prob_cum)
+        action = getAction(a)
+        print("actions : " ,action)
         obs, reward, done, info = env.step(action)
         if done:
 
@@ -43,17 +49,17 @@ if __name__ == "__main__":
     f.close()
     print("Loaded...")
 
-    print("[+] Loading Hyper Parameters 2....")
-    f = open("hyper_param_P2.p","rb")
-    hyper_param2 = pickle.load(f)
-    f.close()
-    print("Loaded....")
+    # print("[+] Loading Hyper Parameters 2....")
+    # f = open("hyper_param_P2.p","rb")
+    # hyper_param2 = pickle.load(f)
+    # f.close()
+    # print("Loaded....")
 
 
     p2 = Process(target =main, args=(hyper_param,) )
-    p3 = Process(target= main, args=(hyper_param2,) )
+    #p3 = Process(target= main, args=(hyper_param2,) )
     p2.start()
-    p3.start()
+    #p3.start()
 
 
 